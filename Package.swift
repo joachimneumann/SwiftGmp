9786c096260c845a58901400d860710fb8c45d62
Joachim Neumann <joachim@joachimneumann.com>
2024-09-23 09:25:39 +0700

    working swift package

--- Package.swift
+++ Package.swift
@@ -6,19 +6,25 @@ import PackageDescription
 let package = Package(
     name: "SwiftGmp",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftGmp",
-            targets: ["SwiftGmp"]),
+            targets: ["SwiftGmp", "MyCTarget"]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "SwiftGmp"),
-        .testTarget(
-            name: "SwiftGmpTests",
-            dependencies: ["SwiftGmp"]
+        .binaryTarget(
+            name: "swiftgmp",
+            path: "./swiftgmp.xcframework"
         ),
+        .target(
+            name: "SwiftGmp",
+            dependencies: ["swiftgmp", "MyCTarget"]
+        ),
+        .target(
+            name: "MyCTarget",
+            dependencies: [],
+            path: "Sources/MyCTarget",
+            publicHeadersPath: "include"
+        )
     ]
 )
--- Sources/SwiftGmp/SwiftGmp.swift
+++ Sources/SwiftGmp/SwiftGmp.swift
@@ -1,2 +1,13 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Foundation
+import MyCTarget
+
+@MainActor var globalUnsignedLongInt: CUnsignedLong = 0
+
+@MainActor
+class SwiftGmp {
+    private var bits: Int = 0
+    private(set) var precision: Int = 0
+    
+    /// init with zeros. The struct will be initialized correctly in init() with mpfr_init2()
+    private var mpfr: mpfr_t = mpfr_t(_mpfr_prec: 0, _mpfr_sign: 0, _mpfr_exp: 0, _mpfr_d: &globalUnsignedLongInt)
+}
