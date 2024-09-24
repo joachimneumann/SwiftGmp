3645f9447955efd33f66b3b6aed1a40741ae2303
Joachim Neumann <joachim@joachimneumann.com>
2024-09-24 18:06:08 +0700

    better async

--- Sources/SwiftGmp/Number.swift
+++ Sources/SwiftGmp/Number.swift
@@ -10,7 +10,15 @@ import Foundation
 
 public func rez(_ n: Number) async -> Number { let ret = await n.copy(); await ret.inplace_rez(); return ret }
 
-public actor Number {
+public actor Number: @preconcurrency CustomDebugStringConvertible  {
+    public var debugDescription: String {
+        if let _str {
+            return "\(_str) precision \(precision) string"
+        }
+        return "\(_swiftGmp!.toDouble())  precision \(precision) gmp"
+    }
+
+    
     public private(set) var precision: Int = 0
     
     init(_ str: String, precision: Int) {
@@ -40,19 +48,18 @@ public actor Number {
     private var _str: String?
     private var _swiftGmp: SwiftGmp?
     
-    private var isStr: Bool { get async { _str != nil } }
+    private var isStr: Bool { _str != nil }
     private var isSwiftGmp: Bool { _swiftGmp != nil }
     private var str: String? { return _str }
     private var swiftGmp: SwiftGmp {
-        get async {
-            if await isStr {
-                _swiftGmp = SwiftGmp(withString: str!, precision: precision)
-                _str = nil
-            }
-            return _swiftGmp!
+        if isStr {
+            _swiftGmp = SwiftGmp(withString: str!, precision: precision)
+            _str = nil
         }
+        return _swiftGmp!
     }
 
+    
     public static func ==(lhs: Number, rhs: Number) async -> Bool {
         if await lhs.precision != rhs.precision { return false }
 
@@ -119,66 +126,66 @@ public actor Number {
 
     public var isValid: Bool {
         get async {
-            if await isStr { return true }
-            return await swiftGmp.isValid
+            if isStr { return true }
+            return swiftGmp.isValid
         }
     }
-    fileprivate func copy() async -> Number {
-        if await isStr {
+    fileprivate func copy() -> Number {
+        if isStr {
             return Number(str!, precision: precision)
         } else {
-            return await Number(swiftGmp.copy())
+            return Number(swiftGmp.copy())
         }
     }
 
-    public func isApproximately(_ other: Double, precision: Double = 1e-3) async -> Bool {
-        return await abs(self.swiftGmp.toDouble() - other) <= precision
+    public func isApproximately(_ other: Double, precision: Double = 1e-3) -> Bool {
+        return abs(self.swiftGmp.toDouble() - other) <= precision
     }
 
     //
     // constants
     // public implementation in numbers.swift
     //
-    func π() async    { await swiftGmp.π() }
-    func e() async    { await swiftGmp.e() }
-    func rand() async { await swiftGmp.rand() }
+    func π() async    { swiftGmp.π() }
+    func e() async    { swiftGmp.e() }
+    func rand() async { swiftGmp.rand() }
 
     //
     // inplace
     //
-    public func inplace_abs() async { await swiftGmp.abs() }
-    public func inplace_sqrt() async { await swiftGmp.sqrt() }
-    public func inplace_sqrt3() async { await swiftGmp.sqrt3() }
-    public func inplace_Z() async { await swiftGmp.Z() }
-    public func inplace_ln() async { await swiftGmp.ln() }
-    public func inplace_log10() async { await swiftGmp.log10() }
-    public func inplace_log2() async { await swiftGmp.log2() }
-    public func inplace_sin() async { await swiftGmp.sin() }
-    public func inplace_cos() async { await swiftGmp.cos() }
-    public func inplace_tan() async { await swiftGmp.tan() }
-    public func inplace_asin() async { await swiftGmp.asin() }
-    public func inplace_acos() async { await swiftGmp.acos() }
-    public func inplace_atan() async { await swiftGmp.atan() }
-    public func inplace_sinh() async { await swiftGmp.sinh() }
-    public func inplace_cosh() async { await swiftGmp.cosh() }
-    public func inplace_tanh() async { await swiftGmp.tanh() }
-    public func inplace_asinh() async { await swiftGmp.asinh() }
-    public func inplace_acosh() async { await swiftGmp.acosh() }
-    public func inplace_atanh() async { await swiftGmp.atanh() }
-    public func inplace_pow_x_2() async { await swiftGmp.pow_x_2() }
-    public func inplace_pow_e_x() async { await swiftGmp.pow_e_x() }
-    public func inplace_pow_10_x() async { await swiftGmp.pow_10_x() }
-    public func inplace_changeSign() async { await swiftGmp.changeSign() }
-    public func inplace_pow_x_3() async { await swiftGmp.pow_x_3() }
-    public func inplace_pow_2_x() async { await swiftGmp.pow_2_x() }
-    public func inplace_rez() async { await swiftGmp.rez() }
-    public func inplace_fac() async { await swiftGmp.fac() }
-    public func inplace_sinD() async { await swiftGmp.sinD() }
-    public func inplace_cosD() async { await swiftGmp.cosD() }
-    public func inplace_tanD() async { await swiftGmp.tanD() }
-    public func inplace_asinD() async { await swiftGmp.asinD() }
-    public func inplace_acosD() async { await swiftGmp.acosD() }
-    public func inplace_atanD() async { await swiftGmp.atanD() }
+    public func inplace_abs() async { swiftGmp.abs() }
+    public func inplace_sqrt() async { swiftGmp.sqrt() }
+    public func inplace_sqrt3() async { swiftGmp.sqrt3() }
+    public func inplace_Z() async { swiftGmp.Z() }
+    public func inplace_ln() async { swiftGmp.ln() }
+    public func inplace_log10() async { swiftGmp.log10() }
+    public func inplace_log2() async { swiftGmp.log2() }
+    public func inplace_sin() async { swiftGmp.sin() }
+    public func inplace_cos() async { swiftGmp.cos() }
+    public func inplace_tan() async { swiftGmp.tan() }
+    public func inplace_asin() async { swiftGmp.asin() }
+    public func inplace_acos() async { swiftGmp.acos() }
+    public func inplace_atan() async { swiftGmp.atan() }
+    public func inplace_sinh() async { swiftGmp.sinh() }
+    public func inplace_cosh() async { swiftGmp.cosh() }
+    public func inplace_tanh() async { swiftGmp.tanh() }
+    public func inplace_asinh() async { swiftGmp.asinh() }
+    public func inplace_acosh() async { swiftGmp.acosh() }
+    public func inplace_atanh() async { swiftGmp.atanh() }
+    public func inplace_pow_x_2() async { swiftGmp.pow_x_2() }
+    public func inplace_pow_e_x() async { swiftGmp.pow_e_x() }
+    public func inplace_pow_10_x() async { swiftGmp.pow_10_x() }
+    public func inplace_changeSign() async { swiftGmp.changeSign() }
+    public func inplace_pow_x_3() async { swiftGmp.pow_x_3() }
+    public func inplace_pow_2_x() async { swiftGmp.pow_2_x() }
+    public func inplace_rez() async { swiftGmp.rez() }
+    public func inplace_fac() async { swiftGmp.fac() }
+    public func inplace_sinD() async { swiftGmp.sinD() }
+    public func inplace_cosD() async { swiftGmp.cosD() }
+    public func inplace_tanD() async { swiftGmp.tanD() }
+    public func inplace_asinD() async { swiftGmp.asinD() }
+    public func inplace_acosD() async { swiftGmp.acosD() }
+    public func inplace_atanD() async { swiftGmp.atanD() }
     
     //
     // twoOperant
@@ -199,7 +206,7 @@ public actor Number {
     }
     
     public func append(_ digit: String) async {
-        if await !isStr {
+        if !isStr {
             _str = digit
             _swiftGmp = nil
         } else if _str == "0" {
@@ -209,7 +216,7 @@ public actor Number {
         }
     }
     
-    public func appendDot() async {
+    public func appendDot() {
         if var _str {
             if !_str.contains(".") { _str.append(".") }
         } else {
@@ -221,11 +228,11 @@ public actor Number {
             if let _str {
                 return _str.starts(with: "-")
             } else {
-                return await swiftGmp.isNegative()
+                return swiftGmp.isNegative()
             }
         }
     }
-    public func changeSign() async {
+    public func changeSign() {
         if var _str {
             if _str == "0" { return }
             if _str.starts(with: "-") {
@@ -234,19 +241,10 @@ public actor Number {
                 _str = "-" + _str
             }
         } else {
-            await swiftGmp.changeSign()
+            swiftGmp.changeSign()
         }
     }
-    
-    public var debugDescription: String {
-        get async {
-            if let _str {
-                return "\(_str) precision \(precision) string"
-            }
-            return "\(await swiftGmp.toDouble())  precision \(precision) gmp"
-        }
-    }
-    
+        
     static func internalPrecision(for precision: Int) -> Int {
         // return precision
         if precision <= 500 {
--- Tests/NumberTests/NumberTests.swift
+++ Tests/NumberTests/NumberTests.swift
@@ -13,29 +13,31 @@ import SwiftGmp
     let numbers = Numbers(precision: 20)
     
     var x1 = await numbers.π
-    #expect(await x1.isApproximately(3.1415926))
+    #expect(x1.isApproximately(3.1415926))
     let x2 = await numbers.new("3")
-    #expect(await x2.isApproximately(3))
+    #expect(x2.isApproximately(3))
     let x3 = await x1 + x2
-    #expect(await x1.isApproximately(3.1415926))
-    #expect(await x2.isApproximately(3))
-    #expect(await x3.isApproximately(6.1415926))
+    #expect(x1.isApproximately(3.1415926))
+    #expect(x2.isApproximately(3))
+    #expect(x3.isApproximately(6.1415926))
     let x4 = await rez(x3)
-    #expect(await x1.isApproximately(3.1415926))
-    #expect(await x2.isApproximately(3))
-    #expect(await x3.isApproximately(6.1415926))
-    #expect(await x4.isApproximately(0.1628242))
+    #expect(x1.isApproximately(3.1415926))
+    #expect(x2.isApproximately(3))
+    #expect(x3.isApproximately(6.1415926))
+    #expect(x4.isApproximately(0.1628242))
     #expect(await rez(x4).isApproximately(6.1415926))
-    #expect(await x4.isApproximately(0.1628242))
+    #expect(x4.isApproximately(0.1628242))
     await x4.inplace_rez()
-    #expect(await x4.isApproximately(6.1415926))
+    #expect(x4.isApproximately(6.1415926))
     await x4.inplace_rez()
-    #expect(await x4.isApproximately(0.1628242))
+    #expect(x4.isApproximately(0.1628242))
 
     x1 = await x1 + x2
-    #expect(await x1.isApproximately(6.14159))
+    #expect(x1.isApproximately(6.14159))
     #expect(!(await x3 == x4))
     let x5 = await 1.0 / x3
+    print(x5)
     #expect(await x5 == x4)
+    print("await x5.debugDescription")
 }
 
